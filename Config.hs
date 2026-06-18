19e51cc9abffa7a34685974fe2d9317119f78243
Fernando Romay <fernando.romay@ruzaani.com>
2026-06-17 22:15:39 -0600

    Load env in Config, use Paths.domain for single source of truth

--- Config.hs
+++ Config.hs
@@ -6,7 +6,9 @@ module Config
 import Data.Text (Text)
 import Data.Maybe (fromMaybe)
 import System.Environment (lookupEnv)
+import Lurk.Env (loadEnv)
 import Text.Read (readMaybe)
+import Paths qualified as P (domain)
 
 data Config = Config
     { port   :: Int
@@ -18,9 +20,11 @@ defaultPort = 3003
 
 loadConfig :: IO Config
 loadConfig = do
+    loadEnv
+    --loadEnvFile "config/secrets.env"
     portStr <- lookupEnv "PORT"
     let port = fromMaybe defaultPort (portStr >>= readMaybe)
     pure Config
         { port   = port
-        , domain = "https://ruzaani.com"
+        , domain = P.domain
         }
